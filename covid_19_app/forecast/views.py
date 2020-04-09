3f7616a6c4ee6426dccb7ea7bf0e8950bb52187d
SlavaPasedko <mefi-100@tut.by>
2020-04-09 16:53:25 +0300

    change

--- covid_19_app/forecast/views.py
+++ covid_19_app/forecast/views.py
@@ -12,6 +12,7 @@ def generate_file_path(country):
 
 @api_view(['GET'])
 def stats_per_country(request):
+    response_data = {}
     serializer = CountryNameSerializer(data=request.data)
     serializer.is_valid(raise_exception=400)
     country = serializer.validated_data['country']
@@ -19,4 +20,5 @@ def stats_per_country(request):
     df = pd.read_csv(filePath, parse_dates=True)
     df = df.drop(df.columns[[0]], axis=1)
     result = df.to_json(orient='records', date_format='iso')
-    return Response(result, 200)
+    response_data['result'] = result
+    return Response(response_data, 200)
